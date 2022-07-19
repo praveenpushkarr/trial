f692cf7a1b8531a74a9a048e6c4292e58acdd87b
Praveen Pushkar <praveenpushkar140@gmail.com>
2022-07-19 23:27:46 +0530

    Updates

---
+++ precache-manifest.3871a0fb0cb616257a48d476830f18ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "01bd726a1f5b3259a906",
+    "url": "/trial/static/css/main.5ef2a002.chunk.css"
+  },
+  {
+    "revision": "01bd726a1f5b3259a906",
+    "url": "/trial/static/js/main.01bd726a.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/trial/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/trial/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "8894df3c3fe9ec8b773b",
+    "url": "/trial/static/js/runtime~main.8894df3c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/trial/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "64587d041a94b74b75e706fd4ad0204d",
+    "url": "/trial/index.html"
+  }
+];
